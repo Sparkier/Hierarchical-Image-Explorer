4512f78e74577dcca57577cd409d747d4530b029
ng28 <niklas.gund@uni-ulm.de>
2021-12-13 21:20:44 +0100

    feat: initial commit of baseline webclient

---
+++ baselineWebclient/README.md
@@ -0,0 +1,6 @@
+# Baseline Webclient    
+## Install
+Run `npm install` to install dependencies
+## Running
+Run `npm run dev` for development build <br>
+Requires a running backend to be usable.
---
+++ baselineWebclient/src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Tableview from "./components/Tableview.svelte";
+</script>
+
+<main>
+	<h1>Welcome to our website. We will present our hierarchical image explorer soon</h1>
+	<p>This is a project by Niklas Gund, Niklas Weidl and Daniel Stucke @Ulm University</p>
+	<div class="container">
+		<Tableview />
+	</div>
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #d87472;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
