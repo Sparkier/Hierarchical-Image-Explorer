a27de561cf66b8d500749ac30fcc4887ad31441b
ng28 <niklas.gund@uni-ulm.de>
2021-12-14 10:24:11 +0100

    refactor: data_provider cleanup

--- backend/mnist_data_provider.py
+++ backend/mnist_data_provider.py
@@ -2,17 +2,15 @@
 # Generates an annotation file by parsing the annotation from the file structure of the zip
 
 import argparse
-import os
 import urllib.request
 import zipfile
 import csv
 from pathlib import Path
 
-extractedFileStructure = "mnist_jpg/MNIST-JPG-testing"
+extractedFileStructure = Path("mnist_jpg/MNIST-JPG-testing")
 
 def download_data(files_path):
     print("Downloading Data")
-    files_path = Path(files_path)
     mnist_test_jpg_url = "https://github.com//teavanist/MNIST-JPG/blob/master/MNIST%20Dataset%20JPG%20format.zip?raw=true"
     zip_filename = "mnistTest.zip"
     urllib.request.urlretrieve(mnist_test_jpg_url, zip_filename)
@@ -23,22 +21,21 @@ def download_data(files_path):
                 zip_file.extract(file, path=files_path)
 
     print("Renaming Folders")
-    os.rename(files_path / "MNIST Dataset JPG format",files_path /  "mnist_jpg")
-    os.rename(files_path / "mnist_jpg/MNIST - JPG - testing", files_path / extractedFileStructure)
+    (files_path / "MNIST Dataset JPG format").rename(files_path /  "mnist_jpg")
+    (files_path / "mnist_jpg/MNIST - JPG - testing").rename(files_path / extractedFileStructure)
     print("Cleaning Up")
-    os.remove(zip_filename)
+    Path(zip_filename).unlink()
 
 def parse_annotations(files_path):
     print("Parsing annotations to compatible format")
-    files_path = Path(files_path)
     
     csv_file = open(files_path / "mnist_test_swg.csv", "w",newline='', encoding="utf8")
     writer = csv.writer(csv_file)
-    writer.writerow(["image_id","file_path","label"])
+    writer.writerow(["image_id", "file_path", "label"])
 
     class_names = Path(files_path / extractedFileStructure).glob('*')
     for class_name in class_names:
-        sub_path = Path(Path(extractedFileStructure) / class_name.name)
+        sub_path = Path(extractedFileStructure / class_name.name)
         path = files_path / sub_path      
         files = Path(path).glob('*')
         for file in files:
@@ -51,7 +48,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--path', help='Path to download the files and generate the annotations to', default="data/mnist", type=str)
     args = parser.parse_args()
     
-    download_data(args.path)
-    parse_annotations(args.path)
+    download_data(Path(args.path))
+    parse_annotations(Path(args.path))
     print("Dataset setup complete")
 
