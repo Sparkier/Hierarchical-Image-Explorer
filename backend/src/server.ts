fb7f9d60e9be9cb98297dbf99eed8752665cb6c2
ng28 <niklas.gund@uni-ulm.de>
2021-12-14 14:14:26 +0100

    small cleanup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-const express = require("express")       
+const express = require("express")
 const bodyParser = require("body-parser")
-const cors = require("cors")             
+const cors = require("cors")
 const fs = require("fs")
 const csv = require("fast-csv")
 
@@ -18,75 +18,71 @@ app.use(express.urlencoded({ extended: true }));
 console.log("server is alive!")
 
 type mnistDatum = {
-    image_id:string,
-    path:string,
-    label:number
+    image_id: string,
+    path: string,
+    label: number
 }
 
 // load data
-var dataFrame:[mnistDatum?] = []
+let dataFrame: mnistDatum[] = []
 fs.createReadStream("../data/mnist/mnist_test_swg.csv")
-    .pipe(csv.parse({headers: true}))
+    .pipe(csv.parse({ headers: true }))
     .on('error', e => console.log(e))
     .on('data', row => dataFrame.push(row))
     .on('end', rowCount => console.log("CSV read with " + rowCount + " rows"))
 
 // functions
-function pathFromId(id){
-    return getElemntByID(id).path
+function getPathFromId(id): string {
+    return getDatumByID(id).path
 }
 
-function getElemntByID(id):mnistDatum{
+function getDatumByID(id): mnistDatum {
     if (dataFrame.length == 0) throw new Error("Dataframe empty")
-    const resultSet = dataFrame.filter(elem => elem!.image_id == id)
-    if(resultSet.length == 0) throw new Error("Id not in dataframe")
-    return resultSet[0]!
-    
-
+    const resultSet = dataFrame.filter(elem => elem.image_id == id)
+    if (resultSet.length == 0) throw new Error("Id not in dataframe")
+    return resultSet[0]
 }
 
-function getAllIds(){
-    let allIDs:[string?] = []
+function getAllIds(): string[] {
+    let allIDs: string[] = []
     dataFrame.forEach(datum => {
-        allIDs.push(datum!.image_id)
+        allIDs.push(datum.image_id)
     });
     return allIDs
 }
 
-// paths
-app.get("/", (req,res) => {
-    console.log("Request received")
+// endpoints
+app.get("/", (req, res) => {
     res.send("Server is running and listening on port " + port)
 })
 
-app.get("/data/images/:id", (req,res) => {
-    let path = pathFromId(req.params.id)
+app.get("/data/images/:id", (req, res) => {
+    let path = getPathFromId(req.params.id)
     let absolutPath = __dirname + imagePath + path
     res.sendFile(absolutPath)
 })
 
-app.get("/data/annotations/:id", (req,res) => {
-    res.send(getElemntByID(req.params.id))
+app.get("/data/annotations/:id", (req, res) => {
+    res.send(getDatumByID(req.params.id))
 })
 
-app.get("/data/allIds", (req,res) => {
+app.get("/data/allIds", (req, res) => {
     res.send(getAllIds())
 })
 
-app.get("/annotations/pages/:id", (req,res) => {
-    let batchSize = 20;
-    console.log("Req Recieved")
-    res.send(dataFrame.slice(req.params.id*batchSize,req.params.id*batchSize+20))
+app.get("/annotations/pages/:id", (req, res) => {
+    const batchSize = 20; // rows per page
+    res.send(dataFrame.slice(req.params.id * batchSize, req.params.id * batchSize + batchSize))
 })
 
-app.get("/data/heads", (req,res) => {
-    if(dataFrame.length > 0){
-        res.send(Object.keys(!dataFrame[0]))
+app.get("/data/heads", (req, res) => {
+    if (dataFrame.length > 0) {
+        res.send(Object.keys(dataFrame[0]))
     }
     else throw new Error("Empty dataframe")
 })
 
-app.get("/test/:number", (req,res) => {
+app.get("/test/:number", (req, res) => {
     res.send(req.params.number)
 })
 
