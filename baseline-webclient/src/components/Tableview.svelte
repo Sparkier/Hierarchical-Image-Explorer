41b003d252cd6c6c9e7a05663b6f35dc80821020
ng28 <niklas.gund@uni-ulm.de>
2022-01-11 11:56:45 +0100

    refactor: orderd the file correctly

--- baseline-webclient/src/components/Tableview.svelte
+++ baseline-webclient/src/components/Tableview.svelte
@@ -29,36 +29,6 @@ $: data = [
 ];
 </script>
 
-<style>
-main {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
-
-ul {
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-  display: flex;
-  flex-direction: column;
-  border-radius: 2px;
-  width: 100%;
-  max-width: 700px;
-  max-height: 800px;
-  background-color: white;
-  overflow-x: scroll;
-  list-style: none;
-  padding: 0;
-}
-
-tr:hover {
-  background-color: #eeeeee;
-}
-</style>
-
 <h1>Data Viewer</h1>
 <main>
   <ul>
@@ -82,3 +52,33 @@ tr:hover {
           on:loadMore={() => {fetchData()}} />
   </ul>
 </main>
+
+<style>
+  main {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  
+  ul {
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+        0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+    display: flex;
+    flex-direction: column;
+    border-radius: 2px;
+    width: 100%;
+    max-width: 700px;
+    max-height: 800px;
+    background-color: white;
+    overflow-x: scroll;
+    list-style: none;
+    padding: 0;
+  }
+  
+  tr:hover {
+    background-color: #eeeeee;
+  }
+  </style>
