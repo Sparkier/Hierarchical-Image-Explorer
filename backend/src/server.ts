e872e922cf0b9601c4d51c8c3ff37405caa3ad1f
ng28 <niklas.gund@uni-ulm.de>
2022-01-11 11:37:58 +0100

    refactor:linting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import fs from "fs";
-import * as csv from "fast-csv";
-import path from "path";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fs from 'fs';
+import * as csv from 'fast-csv';
+import path from 'path';
 
 const dataPath = './data/mnist/';
 
@@ -24,18 +24,20 @@ type mnistDatum = {
   label: number;
 };
 
-type mnistDatumWithID ={
-  image_id: string,
-  file_path: string,
-  label:number
-}
+type mnistDatumWithID = {
+  image_id: string;
+  file_path: string;
+  label: number;
+};
 
 // load data
-const dataFrame: Map<string,mnistDatum> = new Map();
+const dataFrame: Map<string, mnistDatum> = new Map();
 fs.createReadStream(`${dataPath}mnist_test_swg.csv`)
   .pipe(csv.parse({ headers: true }))
   .on('error', (e) => console.log(e))
-  .on('data', (row) => dataFrame.set(row.image_id,{file_path:row.file_path,label:row.label}))
+  .on('data', (row) =>
+    dataFrame.set(row.image_id, { file_path: row.file_path, label: row.label })
+  )
   .on('end', (rowCount) => {
     if (dataFrame.size == 0) throw new Error('Dataset empty');
     console.log('CSV read with ' + rowCount + ' rows');
@@ -46,18 +48,18 @@ function getPathFromId(id): string {
   return getDatumByID(id).file_path;
 }
 
-function combineDatumWithID(datum:mnistDatum, id:string):mnistDatumWithID{
-  return {image_id:id,file_path:datum.file_path,label:datum.label}
+function combineDatumWithID(datum: mnistDatum, id: string): mnistDatumWithID {
+  return { image_id: id, file_path: datum.file_path, label: datum.label };
 }
 
 function getDatumByID(id): mnistDatumWithID {
-  const result = dataFrame.get(id)
-  if (result == undefined) throw new Error("ID not found")
-  else return combineDatumWithID(result,id)
+  const result = dataFrame.get(id);
+  if (result == undefined) throw new Error('ID not found');
+  else return combineDatumWithID(result, id);
 }
 
 function getAllIds(): string[] {
-  return [...dataFrame.keys()]
+  return [...dataFrame.keys()];
 }
 
 // endpoints
@@ -80,17 +82,23 @@ app.get('/data/allIds', (req, res) => {
 });
 
 app.get('/data/label/:id', (req, res) => {
-  const filteredResult = [...dataFrame.entries()].filter((d) => d[1].label === req.params.id);
+  const filteredResult = [...dataFrame.entries()].filter(
+    (d) => d[1].label === req.params.id
+  );
   res.send(filteredResult.map((row) => row[0]));
 });
 
 app.get('/annotations/pages/:id', (req, res) => {
   const batchSize = 20; // rows per page
-  let result = [...dataFrame].slice(
+  const result = [...dataFrame].slice(
     req.params.id * batchSize,
     req.params.id * batchSize + batchSize
-  )
-  res.send(result.map(e => {return combineDatumWithID(e[1],e[0])}));
+  );
+  res.send(
+    result.map((e) => {
+      return combineDatumWithID(e[1], e[0]);
+    })
+  );
 });
 
 app.get('/data/heads', (req, res) => {
