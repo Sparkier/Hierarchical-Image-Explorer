827677d5192c119decbef151077d7657e3c4d1ce
ng28 <niklas.gund@uni-ulm.de>
2022-01-11 11:55:52 +0100

    chore: fixed indentation

--- baseline-webclient/src/App.svelte
+++ baseline-webclient/src/App.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
-	import Tableview from "./components/Tableview.svelte";
+  import Tableview from './components/Tableview.svelte';
 </script>
 
 <main>
-	<h1>Welcome to our website. We will present our hierarchical image explorer soon</h1>
-	<p>This is a project by Niklas Gund, Niklas Weidl and Daniel Stucke @Ulm University</p>
-	<div class="container">
-		<Tableview />
-	</div>
-	
+  <h1>
+    Welcome to our website. We will present our hierarchical image explorer soon
+  </h1>
+  <p>
+    This is a project by Niklas Gund, Niklas Weidl and Daniel Stucke @Ulm
+    University
+  </p>
+  <div class="container">
+    <Tableview />
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #d87472;
-		font-size: 2em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #d87472;
+    font-size: 2em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
