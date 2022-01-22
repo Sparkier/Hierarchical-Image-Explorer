bb0cf42435f1bff184749210f949808d2acf9004
ng28 <niklas.gund@uni-ulm.de>
2022-01-22 13:40:42 +0100

    feat: py script for hirarchical clustering

---
+++ backend/mnist_hirarchicalClustering.py
@@ -0,0 +1,53 @@
+import csv
+from PIL import Image
+from numpy import asarray
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+import json
+import itertools
+
+# read in annotations
+annotations = []
+
+with open('data/mnist/mnist_test_swg.csv') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    for row in csv_reader:
+        annotations.append(row)
+# remove heads
+del annotations[0]
+print("Opened Annotation File")
+
+
+images = []
+
+
+print("Converting images to Numpy Arrays")
+# load images from disk and convert them:
+for annotation in annotations:
+    image = Image.open(annotation[1])
+    data = asarray(image).reshape(1,-1) # from 2D => 1D
+    images.append(data[0])
+
+
+
+print("Started Clustering")
+model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+model.fit(images)
+
+print("Saving results")
+ii = itertools.count(len(images))
+treeview = [{'node_id': int(next(ii)), 'left':int(x[0]), 'right':int(x[1])} for x in model.children_]
+
+labels = []
+clusters = []
+
+
+for i in range(0,len(annotations)):
+    labels.append({"ID":annotations[i][0],"clusterID":i})
+
+
+# save result
+with open('data/mnist/ClusteringTree.json','w') as json_file:
+    json.dump({"tree":treeview,"clusters":labels},json_file, indent=4)
+
+print("Done")
