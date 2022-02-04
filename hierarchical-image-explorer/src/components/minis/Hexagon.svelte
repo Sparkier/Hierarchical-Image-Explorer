421fa868d2e3380fc381d308cc3b49b02b29e5f6
nik-weidl <69912534+nik-weidl@users.noreply.github.com>
2022-02-04 13:54:21 +0100

    chore: linting

--- hierarchical-image-explorer/src/components/minis/Hexagon.svelte
+++ hierarchical-image-explorer/src/components/minis/Hexagon.svelte
@@ -1,65 +1,65 @@
 <script lang="ts">
 
-	import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from 'svelte';
 
-	const dispatch = createEventDispatcher();
-	export let side:number = 100;
-	export let color:string = "green";
-	export let image:string = "";
-	export let scale:number = 1;
-	export let x:number = 0;
-	export let y:number = 0;
-	export let text:string = "";
-	//export let textSize:string = "20";
-	
-	const t = 120 * Math.PI / 180; // 120 degrees in radians
-	const a = (side*Math.sqrt(3))/2; // distance from a side to center of hexagon
+  const dispatch = createEventDispatcher();
+  export let side:number = 100;
+  export let color:string = "green";
+  export let image:string = "";
+  export let scale:number = 1;
+  export let x:number = 0;
+  export let y:number = 0;
+  export let text:string = "";
+  //export let textSize:string = "20";
+  
+  const t = 120 * Math.PI / 180; // 120 degrees in radians
+  const a = (side*Math.sqrt(3))/2; // distance from a side to center of hexagon
 
-	const Left ={x: 0, y: a}
-	const BottomLeft ={x:Left.x-(side*Math.cos(t)), y:Left.y+(side*Math.sin(t))}
-	const TopLeft ={x:BottomLeft.x, y:0}
-	const BottomRight ={x:BottomLeft.x+side, y:BottomLeft.y}
-	const Right ={x:side*2, y:a}
-	const TopRight ={x:BottomLeft.x+side, y:0}
-	
-	const l = `${Left.x},${Left.y}`
-	const bl = `${BottomLeft.x},${BottomLeft.y}`
-	const tl = `${TopLeft.x},${TopLeft.y}`
-	const br = `${BottomRight.x},${BottomRight.y}`
-	const r = `${Right.x},${Right.y}`
-	const tr = `${TopRight.x},${TopRight.y}`
-	
-	$: fill = `url(#image-bg_${image})`
-	if(image == ""){
-		fill = color;
-	}
+  const Left = {x: 0, y: a};
+  const BottomLeft = {x: Left.x-(side*Math.cos(t)), y: Left.y+(side*Math.sin(t))};
+  const TopLeft = {x: BottomLeft.x, y: 0};
+  const BottomRight = {x: BottomLeft.x+side, y: BottomLeft.y};
+  const Right = {x: side*2, y: a};
+  const TopRight = {x: BottomLeft.x+side, y: 0};
+  
+  const l = `${Left.x},${Left.y}`;
+  const bl = `${BottomLeft.x},${BottomLeft.y}`;
+  const tl = `${TopLeft.x},${TopLeft.y}`;
+  const br = `${BottomRight.x},${BottomRight.y}`;
+  const r = `${Right.x},${Right.y}`;
+  const tr = `${TopRight.x},${TopRight.y}`;
+  
+  $: fill = `url(#image-bg_${image})`
+  if(image == ""){
+    fill = color;
+  }
 
-	function handleClick(){
-		dispatch('message', {
-			text: 'Hello!'
-		});
-	}
+  function handleClick(){
+    dispatch('message', {
+      text: 'Hello!'
+    });
+  }
 </script>
 
 <style>
-	.hex{
-		stroke: black;
-  		stroke-width: 1;
-	}
+  .hex{
+    stroke: black;
+      stroke-width: 1;
+  }
 </style>
 
 <defs>
-	<pattern id={`image-bg_${image}`} height="{side*2}" width="{side*2}" patternUnits="userSpaceOnUse">
-		<image width="{side*2}" height="{side*2}" xlink:href={image}/>
-	</pattern>
+  <pattern id={`image-bg_${image}`} height="{side*2}" width="{side*2}" patternUnits="userSpaceOnUse">
+    <image width="{side*2}" height="{side*2}" xlink:href={image}/>
+  </pattern>
 </defs>
 <g transform="scale({scale}) translate({x}, {y})" on:click={handleClick}> 
-	<polygon class="hex" points="{r} {br} {bl} {l} {tl} {tr}" fill="{fill}"/>
-	<text transform="translate({side},{side})" 
-			font-family="Verdana" 
-			font-size="30"
-			text-anchor="middle"
-			fill="red">
-    	{text}
+  <polygon class="hex" points="{r} {br} {bl} {l} {tl} {tr}" fill="{fill}"/>
+  <text transform="translate({side},{side})" 
+      font-family="Verdana" 
+      font-size="30"
+      text-anchor="middle"
+      fill="red">
+      {text}
     </text>
 </g>
