f33876b25603044d451d66b8d7b54cfc7f79c7f7
niklas-gund <94127121+niklas-gund@users.noreply.github.com>
2022-02-04 15:28:59 +0100

    feat: py script for hirarchical clustering (#22)
    * feat: py script for hirarchical clustering
    
    * chore: linting
    
    * refactor: reworked clustering
    
    * chore: cleaned up new lines
    
    * chore added docs
    
    * chore:fixed docstring locations
    
    * chore:added requirements.txt
    
    * chore: added python required packages to glabal requirements.txt
    
    * refactor:renamed outer variables to avoid confusion
    
    Co-authored-by: ng28 <niklas.gund@uni-ulm.de>

---
+++ backend/mnist_hirarchical_clustering.py
@@ -0,0 +1,87 @@
+"""Does basic hierarchical clustering on the mnist test data"""
+
+import argparse
+import json
+import itertools
+import csv
+from PIL import Image
+from numpy import asarray
+from sklearn.cluster import AgglomerativeClustering
+
+
+def read_annoations(datapath):
+    """Reads in a given csv file"""
+    # read in annotations
+    annotations = []
+
+    with open(datapath, encoding='utf-8') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        next(csv_reader)
+        for row in csv_reader:
+            annotations.append(row)
+    print("Opened Annotation File")
+    return annotations
+
+
+def read_image_data(annotations):
+    """Reads images from annotations and converts them to 1D Array"""
+    images = []
+    print("Converting images to Numpy Arrays")
+    # load images from disk and convert them:
+    for annotation in annotations:
+        image = Image.open(annotation[1])
+        data = asarray(image).reshape(1,-1) # from 2D => 1D
+        images.append(data[0])
+    return images
+
+
+
+def cluster_data(images):
+    """Runs Clustering on a given image Array"""
+    print("Started Clustering")
+    model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+    model.fit(images)
+    return model
+
+
+
+def save_clustering(annotations, images, model, output_path):
+    """Saves the resulting hierarchical tree and image labesl to a json file"""
+    print("Saving results")
+    image_iterator = itertools.count(len(images))
+    treeview = [{
+    'node_id': int(next(image_iterator)),
+    'children': [int(x[0]), int(x[1])],
+    } for x in model.children_]
+
+    labels = []
+
+    for index, value in enumerate(annotations):
+        labels.append({"ID":value[0],"clusterID":index})
+
+    with open(output_path,'w', encoding='utf-8') as json_file:
+        json.dump({"tree":treeview,"clusters":labels},json_file, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-a',
+        '--annotation_file',
+        help='Path to annotation file for the dataset',
+        default="data/mnist/mnist_test_swg.csv",
+        type=str)
+    parser.add_argument(
+    '-o',
+    '--output_file',
+    help='Path to output json file',
+    default='data/mnist/ClusteringTree.json',
+    type=str)
+
+    args = parser.parse_args()
+
+    mnist_annotations = read_annoations(args.annotation_file)
+    mnist_images = read_image_data(mnist_annotations)
+    mnist_model = cluster_data(mnist_images)
+    save_clustering(mnist_annotations, mnist_images, mnist_model, args.output_file)
+    print("Done")
