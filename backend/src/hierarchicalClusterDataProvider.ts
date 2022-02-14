bb5dced0f6449850e8333eb01c1d618ee8525184
niklas-gund <94127121+niklas-gund@users.noreply.github.com>
2022-02-14 13:16:41 +0100

    Backend cluster image end points (#27)
    * feat:new endpoints for cluster infos
    
    * chore:linting
    
    * refactor: changed filename concatination
    
    Co-authored-by: ng28 <niklas.gund@uni-ulm.de>

--- backend/src/hierarchicalClusterDataProvider.ts
+++ backend/src/hierarchicalClusterDataProvider.ts
@@ -96,6 +96,18 @@ export default class HierarchicalClusterDataProvider {
 
     throw new Error('Node has no parent');
   }
+
+  public getHierarchicalLevel(nodeID: number): number {
+    let level = 0;
+    let currentNode = this.getNode(nodeID);
+
+    while (!currentNode.isLeaf()) {
+      level++;
+      currentNode = currentNode.getChildren()[0];
+    }
+
+    return level;
+  }
 }
 
 class HcNode {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -87,7 +87,7 @@ app.get('/', (req, res) => {
 
 app.get('/data/images/:id', (req, res) => {
   const file_path = getPathFromId(req.params.id);
-  const absolutPath = path.join(__dirname, '../') + file_path;
+  const absolutPath = path.join(__dirname, '../',file_path);
   res.sendFile(absolutPath);
 });
 
@@ -147,6 +147,34 @@ app.get('/hc/repimage/:id', (req, res) => {
   const file_path = getPathFromId(
     dataIDS[Math.floor(Math.random() * dataIDS.length)]
   );
-  const absolutPath = path.join(__dirname, '../') + file_path;
+  const absolutPath = path.join(__dirname, '../', file_path);
   res.sendFile(absolutPath);
+});
+
+// for testing random image
+app.get('/hc/repimage/close/:id/:rank', (req, res) => {
+  const dataIDS = hcDataProvider.getAllIDs(req.params.id);
+  const file_path = getPathFromId(
+    dataIDS[Math.floor(Math.random() * dataIDS.length)]
+  );
+  const absolutPath = path.join(__dirname, '../', file_path);
+  res.sendFile(absolutPath);
+});
+
+// for testing random image
+app.get('/hc/repimage/distant/:id/:rank', (req, res) => {
+  const dataIDS = hcDataProvider.getAllIDs(req.params.id);
+  const file_path = getPathFromId(
+    dataIDS[Math.floor(Math.random() * dataIDS.length)]
+  );
+  const absolutPath = path.join(__dirname, '../', file_path);
+  res.sendFile(absolutPath);
+});
+
+app.get('/hc/clusterinfo/size/:id', (req, res) => {
+  res.send(hcDataProvider.getAllIDs(req.params.id).length.toString());
+});
+
+app.get('/hc/clusterinfo/level/:id', (req, res) => {
+  res.send(hcDataProvider.getHierarchicalLevel(req.params.id).toString());
 });
