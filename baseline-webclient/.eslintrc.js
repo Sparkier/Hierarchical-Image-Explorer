b443641549eadf99b6937c314f8b06dfbd4404fb
onc <c.onzenoodt@gmail.com>
2022-02-16 18:50:31 +0100

    Linting (#28)
    * Move linting dependencies into the individual workspaces
    
    * Moving typescript configs
    
    * Moving prettier config
    
    * Moving eslint configs
    
    * Cleanup package.json script sections
    
    * Adding `yarn` format
    
    * Running `yarn format`
    
    * Bring back `build` target
    
    * Remove `.svelte` from the eslint `parserOptions` to fix linting

---
+++ baseline-webclient/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["svelte3", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  env: {
+    es6: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+  settings: {
+    "svelte3/typescript": require("typescript"),
+  },
+  ignorePatterns: ['node_modules']
+};
