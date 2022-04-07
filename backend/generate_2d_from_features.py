80a1d95d5684a1c315d53ebe37d737d10902321c
ng28 <niklas.gund@uni-ulm.de>
2022-04-07 10:11:12 +0200

    style: linting + added pandas to requirements

--- backend/generate_2d_from_features.py
+++ backend/generate_2d_from_features.py
@@ -1,11 +1,13 @@
+"""Takes a fature list representation of an image and runs dimensionality reduction to 2D using t-sne"""
 import argparse
-from sklearn.manifold import TSNE
 from pathlib import Path
-import pandas as pd
 import json
+from sklearn.manifold import TSNE
+import pandas as pd
 
 
 def load_features(file_path):
+    """Loads a given feature json file and returns ids and features seperately"""
     with open(file_path, "r", encoding="utf8") as feature_json_file:
         feature_data = json.load(feature_json_file)
         features = []
@@ -17,6 +19,7 @@ def load_features(file_path):
 
 
 def run_tsne(features, ids):
+    """Runs t-sne on a given feature list. Returns a dataframe with id and coordinates"""
     tsne = TSNE(n_components=2, verbose=1, random_state=222, perplexity=32)
     tsne_output = tsne.fit_transform(features)
     data_frame = pd.DataFrame()
@@ -27,18 +30,21 @@ def run_tsne(features, ids):
 
 
 def save_points_data(out_dir, out_name, points_df):
+    """Saves dataframe to given path"""
     Path(out_dir).mkdir(parents=True, exist_ok=True)
     points_df.to_csv(out_name, index=False)
 
 
-def main(args):
+def main(parameters):
+    """Main Method"""
     print("Generating 2D points from feature Vectors")
-    features, ids = load_features(args.feature_json)
+    features, ids = load_features(parameters.feature_json)
     print("Running t-sne")
     points_df = run_tsne(features, ids)
-    out_name = args.feature_json.replace("_feat.json", "") + "_2d_points.csv"
+    out_name = parameters.feature_json.replace(
+        "_feat.json", "") + "_2d_points.csv"
     print("Saving results to file")
-    save_points_data(args.output_dir, out_name, points_df)
+    save_points_data(parameters.output_dir, out_name, points_df)
     print("Done")
 
 
