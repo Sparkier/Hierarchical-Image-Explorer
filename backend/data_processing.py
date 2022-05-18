7bd6281f492efc790583f9f3b47070c7f7cc11d2
ng28 <niklas.gund@uni-ulm.de>
2022-05-18 10:05:21 +0200

    style: replaced ' with "

--- backend/data_processing.py
+++ backend/data_processing.py
@@ -20,8 +20,8 @@ import umap
 def read_annotations(swg_path):
     """Reads annotations from given file"""
     annotation_list = []
-    with open(swg_path, 'r', encoding="utf8") as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
+    with open(swg_path, "r", encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
         next(reader)
         for row in reader:
             annotation_list.append(row)
@@ -30,7 +30,7 @@ def read_annotations(swg_path):
 
 def load_json(file_path):
     """reads the content of a json file and returns it"""
-    with open(file_path, encoding='utf8') as json_file:
+    with open(file_path, encoding="utf8") as json_file:
         return json.load(json_file)
 
 
@@ -63,8 +63,8 @@ def save_clustering_json_agglomerative(annotations, model, output_path):
     print("Saving results")
     image_iterator = itertools.count(len(annotations))
     treeview = [{
-        'node_id': int(next(image_iterator)),
-        'children': [int(x[0]), int(x[1])],
+        "node_id": int(next(image_iterator)),
+        "children": [int(x[0]), int(x[1])],
     } for x in model.children_]
 
     labels = []
@@ -72,7 +72,7 @@ def save_clustering_json_agglomerative(annotations, model, output_path):
     for index, value in enumerate(annotations):
         labels.append({"ID": value[0], "clusterID": index})
 
-    with open(output_path, 'w', encoding='utf-8') as json_file:
+    with open(output_path, "w", encoding="utf-8") as json_file:
         json.dump({"tree": treeview, "clusters": labels}, json_file, indent=4)
 
 
@@ -95,8 +95,8 @@ def save_clustering_json_hdbscan(tree, annotations, output_name):
             "clusterID": int(index)
         })
     to_write = {"tree": tree_list, "clusters": clusters}
-    with open(output_name, 'w',
-              encoding='utf-8') as json_file:
+    with open(output_name, "w",
+              encoding="utf-8") as json_file:
         json.dump(to_write, json_file, indent=2)
 
 
@@ -113,7 +113,7 @@ def pixels_to_features(images):
 def extract_features_vgg_16(images):
     """Takes in images as pixel data and runs a vgg-16
        pretrained on imagenet as a feature extractor"""
-    model = keras.applications.VGG16(weights='imagenet', include_top=True)
+    model = keras.applications.VGG16(weights="imagenet", include_top=True)
     feat_extractor = Model(
         inputs=model.input, outputs=model.get_layer("fc2").output)
     image_features = []
@@ -135,7 +135,7 @@ def run_clustering_agglomerative(similarity_matrix):
     """Runs clustering based on similarity matrix with scikit
          learn agglomerative clustering"""
     model = AgglomerativeClustering(
-        distance_threshold=0, n_clusters=None, affinity='precomputed', linkage="average")
+        distance_threshold=0, n_clusters=None, affinity="precomputed", linkage="average")
     model.fit(similarity_matrix)
     return model
 
@@ -202,7 +202,7 @@ def run_similarity_generation(args, features, file_name_prefix):
         if features is None:
             sys.exit("Generating a similarity matrix requires a feature set. \
                 Please provide either a metric or a path to a previously generated feature list")
-        if args.similarity_metric in ('euclidean', 'cosine'):
+        if args.similarity_metric in ("euclidean", "cosine"):
             similarity_matrix = create_distance_matrix(
                 features, args.similarity_metric)
         else:
@@ -276,9 +276,9 @@ def run_pipeline(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Run processing on image datasets defined by swg.csv files every optional \
+        description="Run processing on image datasets defined by swg.csv files every optional \
           parameter can be replaced with a path \
-          to the corresponding output file of the processing step.')
+          to the corresponding output file of the processing step.")
     parser.add_argument("swg_file", type=str, help="path to swg file")
     parser.add_argument("output_dir", type=str,
                         help="directory to output generated files to")
