673ef9e82a4559824cbf009654718f45fa352494
Niklas Weidl <nik.weidl@gmail.com>
2022-05-24 17:35:19 +0200

    feat: fun with bugs

--- hierarchical-image-explorer/src/components/Accumulator.svelte
+++ hierarchical-image-explorer/src/components/Accumulator.svelte
@@ -3,15 +3,25 @@
   import Hexagon from './minis/Hexagon.svelte';
   import { ColorUtil } from '../services/colorUtil';
   import { onMount } from 'svelte';
+  import ZoomSVG from './ZoomSVG.svelte';
+  import { generateScale } from '../services/scaleUtilities';
 
   export let data: PointData[];
   export let columns = 50;
   export let rows = 80;
 
   let svgWidth: number;
+  const svgHeight = 600;
+  let svg: SVGSVGElement;
+  let g: SVGSVGElement;
+  let svgContainer: HTMLElement;
+
   let quantisedData: PointData[][][] = [];
   let hexaSide = -1;
   const hexaShortDiag = Math.sqrt(3) / 2;
+  let extent: number;
+  let transform: [number, number];
+  let filteredData: PointData[] = [];
 
   $: scaleQuantisedX = (v: number, row: number) => {
     return svgWidth == undefined
@@ -23,9 +33,19 @@
     return hexaShortDiag * hexaSide * v;
   };
 
+  $: scaleY = generateScale(
+    [-100, 100],
+    svgWidth == undefined ? 0 : svgWidth,
+    0
+  );
+  $: scaleX = generateScale([-100, 100], svgWidth, 0);
+
   onMount(() => {
     quantisedData = calculateQuantisation(data);
   });
+
+  // $: filteredData =
+  //   extent > 1 && transform != undefined ? filterPointsBoundingRect() : [];
 
   /**
    * Calculate a quantisation of our values, to combine multiple points
@@ -81,10 +101,65 @@
     });
     return quantised;
   }
+
+  function filterPointsBoundingRect(): PointData[] {
+    console.log('Started filtering');
+    const rect = svgContainer.getBoundingClientRect();
+    const x1_dom = rect.x;
+    const x2_dom = rect.x + rect.width;
+    const y1_dom = rect.y;
+    const y2_dom = rect.y + rect.height;
+
+    const topLeft = svgPoint(svg, x1_dom, y1_dom, g);
+    const bottomRight = svgPoint(svg, x2_dom, y2_dom, g);
+    console.log(topLeft, bottomRight);
+    const filtered = data.filter(
+      (p) =>
+        scaleX(p.x) > topLeft.x &&
+        scaleX(p.x) < bottomRight.x &&
+        scaleY(p.y) > topLeft.y &&
+        scaleY(p.y) < bottomRight.y
+    );
+    console.log('Filtered ' + filtered.length);
+    console.log('Stopped filtering');
+    return filtered;
+  }
+
+  function svgPoint(
+    element: SVGSVGElement,
+    x: number,
+    y: number,
+    group: SVGSVGElement
+  ) {
+    const pt = element.createSVGPoint();
+    pt.x = x;
+    pt.y = y;
+    const matrix = group.getScreenCTM()?.inverse();
+    if (matrix == undefined) throw new Error('Transformation Matrix undefined');
+    console.log(matrix);
+    return pt.matrixTransform(matrix);
+  }
 </script>
 
-<div bind:clientWidth={svgWidth}>
-  <svg class="w-full h-[80vh] mt-10">
+<div
+  bind:clientWidth={svgWidth}
+  bind:this={svgContainer}
+  style="height: {svgHeight}px"
+  class="overflow-hidden"
+>
+  <button on:click={() => (filteredData = filterPointsBoundingRect())}
+    >Click for recalculation</button
+  >
+  <div>
+    Ich bringe frohe Kunde: die Anzahl an gefilterten Bildern ist: {filteredData.length}
+  </div>
+  <ZoomSVG
+    viewBox="0 0 {svgWidth} {svgHeight}"
+    bind:extent
+    bind:transform
+    bind:svg
+    bind:g
+  >
     {#if hexaSide != 0}
       {#each quantisedData as columnsList, x}
         {#each columnsList as cell, y}
@@ -95,9 +170,27 @@
               y={scaleQuantisedY(y)}
               color={ColorUtil.getCellColor(quantisedData[x][y])}
             />
+            {#if true}
+              <!--insert new hexagons-->
+              {#each filteredData as point}
+                <!-- <Hexagon
+                  side={hexaSide / 2}
+                  x={scaleX(point.x)}
+                  y={scaleY(point.y)}
+                  color={'deeppink'}
+                /> -->
+                <circle
+                  cx={scaleX(point.x)}
+                  cy={scaleY(point.y)}
+                  r="5"
+                  fill="deeppink"
+                />
+              {/each}
+            {/if}
           {/if}
         {/each}
       {/each}
     {/if}
-  </svg>
+  </ZoomSVG>
+  <div />
 </div>
---
+++ hierarchical-image-explorer/src/components/ZoomSVG.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from 'svelte';
+  import { zoom } from 'd3-zoom';
+  import { select } from 'd3-selection';
+
+  export let viewBox = '0 0 300 150';
+  export let transform;
+  export let extent;
+  export let g;
+  export let svg;
+  onMount(() => {
+    if (svg && g) {
+      select(svg).call(
+        zoom().on('zoom', ({ transform: transformNew }) => {
+          const { k, x, y } = transformNew;
+          extent = k;
+          transform = [x, y];
+          select(g).attr('transform', `translate(${x}, ${y}) scale(${k})`);
+        })
+      );
+    }
+  });
+</script>
+
+<svg {viewBox} bind:this={svg} id="jens-joachim">
+  <g bind:this={g}>
+    <slot />
+  </g>
+</svg>
