b80f8aa86ac64058036cbe69701f8437b38f5541
Niklas <69912534+nik-weidl@users.noreply.github.com>
2022-07-04 20:39:31 +0200

    Hexagon Rework (Rendering and Borders) (#68)
    * feat: add seperate edge coloring and higher res images rendering
    
    * fix: borders were not connecting properly
    
    * fix: accidentally removed stroke width
    
    * fix: change initialization color
    
    * refactor: capitalization and stroke setting
    
    * fix: remove overlapping

--- hierarchical-image-explorer/src/components/minis/Hexagon.svelte
+++ hierarchical-image-explorer/src/components/minis/Hexagon.svelte
@@ -1,67 +1,82 @@
 <script lang="ts">
+  import Borders from './Borders.svelte';
+
   export let side: number = 100;
-  export let color: string = 'green';
   export let image: string = '';
   export let scale: number = 1;
   export let x: number = 0;
   export let y: number = 0;
   export let text: string = '';
-  export let stroke: string = 'black';
+  export let stroke: string = 'none';
   export let strokeWidth: number = 1;
 
   const t = (120 * Math.PI) / 180; // 120 degrees in radians
-  const a = (side * Math.sqrt(3)) / 2; // distance from a side to center of hexagon
+  $: a = (side * Math.sqrt(3)) / 2; // distance from a side to center of hexagon
 
-  const Left = { x: 0, y: a };
-  const BottomLeft = {
-    x: Left.x - side * Math.cos(t),
-    y: Left.y + side * Math.sin(t),
+  $: left = { x: 0, y: a };
+  $: bottomLeft = {
+    x: left.x - side * Math.cos(t),
+    y: left.y + side * Math.sin(t),
   };
-  const TopLeft = { x: BottomLeft.x, y: 0 };
-  const BottomRight = { x: BottomLeft.x + side, y: BottomLeft.y };
-  const Right = { x: side * 2, y: a };
-  const TopRight = { x: BottomLeft.x + side, y: 0 };
+  $: topLeft = { x: bottomLeft.x, y: 0 };
+  $: bottomRight = { x: bottomLeft.x + side, y: bottomLeft.y };
+  $: right = { x: side * 2, y: a };
+  $: topRight = { x: bottomLeft.x + side, y: 0 };
 
-  const l = `${Left.x},${Left.y}`;
-  const bl = `${BottomLeft.x},${BottomLeft.y}`;
-  const tl = `${TopLeft.x},${TopLeft.y}`;
-  const br = `${BottomRight.x},${BottomRight.y}`;
-  const r = `${Right.x},${Right.y}`;
-  const tr = `${TopRight.x},${TopRight.y}`;
+  $: l = `${left.x},${left.y}`;
+  $: bl = `${bottomLeft.x},${bottomLeft.y}`;
+  $: tl = `${topLeft.x},${topLeft.y}`;
+  $: br = `${bottomRight.x},${bottomRight.y}`;
+  $: r = `${right.x},${right.y}`;
+  $: tr = `${topRight.x},${topRight.y}`;
 
-  $: fill = image == '' ? color : `url(#image-bg_${image})`;
+  // Individual hexagons require individual clip paths
+  $: IDString = Math.round(x * 1000) + '' + Math.round(y * 1000);
 </script>
 
-<defs>
-  <pattern
-    id={`image-bg_${image}`}
-    height={side * 2}
-    width={side * 2}
-    patternUnits="userSpaceOnUse"
-  >
-    <image width={side * 2} height={side * 2} xlink:href={image} />
-  </pattern>
-</defs>
+<!--
+  A polygon is used to create a clipppath for our image. 
+  Then the polygon borders are drawn over the images.
+ -->
 <g
   transform="scale({scale}) translate({x}, {y})"
   on:click
   on:mouseenter
   on:mouseleave
 >
-  <polygon
+  <polygon id={IDString + '_polygon'} points="{r} {br} {bl} {l} {tl} {tr}" />
+
+  <defs>
+    <clipPath id={IDString + '_clipping'}>
+      <use xlink:href="#{IDString + '_polygon'}" />
+    </clipPath>
+  </defs>
+
+  {#if image != ''}
+    <image
+      href={image}
+      width={side * 2}
+      height={side * 2}
+      clip-path="url(#{IDString + '_clipping'})"
+      style="image-rendering: pixelated;"
+    >
+      <text
+        class="pointer-events-none"
+        transform="translate({side},{side})"
+        font-family="Verdana"
+        font-size="30"
+        text-anchor="middle"
+        fill="red"
+      >
+        {text}
+      </text>
+    </image>
+  {/if}
+  <use
+    clip-path="url(#{IDString + '_clipping'})"
+    xlink:href="#{IDString + '_polygon'}"
+    fill="none"
     {stroke}
     stroke-width={strokeWidth}
-    points="{r} {br} {bl} {l} {tl} {tr}"
-    {fill}
   />
-  <text
-    class="pointer-events-none"
-    transform="translate({side},{side})"
-    font-family="Verdana"
-    font-size="30"
-    text-anchor="middle"
-    fill="red"
-  >
-    {text}
-  </text>
 </g>
