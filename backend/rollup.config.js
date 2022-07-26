d5b379a2eaed4b0350f3c261cdfd93f224df9bbc
Christian van Onzenoodt <christian.van-onzenoodt@uni-ulm.de>
2022-07-26 15:06:18 +0200

    Fix backend build with aquero

--- backend/rollup.config.js
+++ backend/rollup.config.js
@@ -1,12 +1,18 @@
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import json from "@rollup/plugin-json"
 
 export default {
-  input: './build-ts/server.js',
+  input: 'src/server.ts',
   output: {
     dir: 'build',
     format: 'cjs'
   },
-  plugins: [json(), commonjs(), nodeResolve()]
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+		typescript(),
+    json(),
+  ]
 };
