f32b4ab4cd50da389ed1bf1f17632316e9302e84
Daniel Stucke <57717665+daniel-stucke@users.noreply.github.com>
2022-08-19 17:53:09 +0200

    feat: changed bar chart to mirrored bar chart + cosmetics (#113)
    Co-authored-by: niklas-gund <niklas-gund@users.noreply.github.com>
    Co-authored-by: Niklas <nik-weidl@users.noreply.github.com>
    
    Co-authored-by: niklas-gund <niklas-gund@users.noreply.github.com>
    Co-authored-by: Niklas <nik-weidl@users.noreply.github.com>

--- hierarchical-image-explorer/src/components/minis/ClusterContentDistChart.svelte
+++ hierarchical-image-explorer/src/components/minis/ClusterContentDistChart.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { VegaLite } from 'svelte-vega';
+  import { Vega } from 'svelte-vega';
   import { ColorUtil } from '../../services/colorUtil';
 
   export let distributionA: { label: string; amount: number }[] = [];
@@ -9,7 +9,8 @@
     amount: number;
     selection: string;
   }[];
-  export let columnName: string;
+
+  let categories: { category: string }[];
 
   $: {
     distribution = [
@@ -30,47 +31,185 @@
     ];
   }
 
-  $: data = {
-    table: distribution,
-  };
+  $: {
+    const categorySet = new Set<string>();
+    distributionA.forEach((e) => categorySet.add(e.label));
+    distributionB.forEach((e) => categorySet.add(e.label));
+    categories = [...categorySet].map((a) => {
+      return { category: a };
+    });
+  }
 
   $: spec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    $schema: 'https://vega.github.io/schema/vega/v5.json',
     name: 'clusterDist',
     description: 'Cluster content distribution',
     background: null,
-    autosize: {
-      type: 'pad',
-      contains: 'padding',
-    },
-    data: {
-      name: 'table',
-    },
-    mark: {
-      type: 'bar',
-    },
-    encoding: {
-      x: {
-        field: 'amount',
-        type: 'quantitative',
+    height: 20 * categories.length,
+    padding: 5,
+    signals: [
+      { name: 'chartWidth', value: 125 },
+      { name: 'chartPad', value: 40 },
+      { name: 'width', update: '2 * chartWidth + chartPad' },
+    ],
+    data: [
+      {
+        name: 'distribution',
+        values: distribution.sort((a, b) => (a.label < b.label ? 1 : -1)),
       },
-      y: { field: 'label', type: 'nominal', title: columnName },
-      color: {
-        field: 'selection',
-        scale: {
-          domain: ['A', 'B'],
-          range: [
-            ColorUtil.SELECTION_HIGHLIGHT_COLOR_A,
-            ColorUtil.SELECTION_HIGHLIGHT_COLOR_B,
-          ],
+      {
+        name: 'selectionA',
+        source: 'distribution',
+        transform: [{ type: 'filter', expr: "datum.selection == 'A'" }],
+      },
+      {
+        name: 'selectionB',
+        source: 'distribution',
+        transform: [{ type: 'filter', expr: "datum.selection == 'B'" }],
+      },
+      {
+        name: 'categories',
+        source: 'distribution',
+        transform: [{ type: 'aggregate', groupby: ['label'] }],
+      },
+    ],
+    scales: [
+      {
+        name: 'y',
+        type: 'band',
+        range: [{ signal: 'height' }, 0],
+        round: true,
+        domain: { data: 'categories', field: 'label' },
+      },
+      {
+        name: 'c',
+        type: 'ordinal',
+        domain: ['A', 'B'],
+        range: [
+          ColorUtil.SELECTION_HIGHLIGHT_COLOR_A,
+          ColorUtil.SELECTION_HIGHLIGHT_COLOR_B,
+        ],
+      },
+    ],
+    marks: [
+      {
+        type: 'text',
+        from: { data: 'categories' },
+        encode: {
+          enter: {
+            x: { signal: 'chartWidth + chartPad /2' },
+            y: { scale: 'y', field: 'label', band: 0.5 },
+            text: { field: 'label' },
+            baseline: { value: 'middle' },
+            align: { value: 'center' },
+            fill: { value: '#000' },
+          },
         },
       },
-      tooltip: [
-        { field: 'label', type: 'nominal' },
-        { field: 'amount', type: 'quantitative' },
-      ],
-    },
+      {
+        type: 'group',
+
+        encode: {
+          update: {
+            x: { value: 0 },
+            height: { signal: 'height' },
+          },
+        },
+        scales: [
+          {
+            name: 'x',
+            type: 'linear',
+            range: [{ signal: 'chartWidth' }, 0],
+            nice: true,
+            zero: true,
+            domain: { data: 'distribution', field: 'amount' },
+          },
+        ],
+
+        axes: [
+          {
+            orient: 'bottom',
+            scale: 'x',
+            format: 's',
+            title: 'Cluster A',
+            tickCount: 5,
+          },
+        ],
+
+        marks: [
+          {
+            type: 'rect',
+            from: { data: 'selectionA' },
+            encode: {
+              enter: {
+                x: { scale: 'x', field: 'amount' },
+                x2: { scale: 'x', value: 0 },
+                y: { scale: 'y', field: 'label' },
+                height: { scale: 'y', band: 1, offset: -1 },
+                fillOpacity: { value: 1 },
+                fill: { scale: 'c', field: 'selection' },
+                tooltip: {
+                  signal:
+                    "{'title': 'Selection A','category': datum.label, 'amount': datum.amount}",
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        type: 'group',
+
+        encode: {
+          update: {
+            x: { signal: 'chartWidth + chartPad' },
+            height: { signal: 'height' },
+          },
+        },
+        scales: [
+          {
+            name: 'x',
+            type: 'linear',
+            range: [0, { signal: 'chartWidth' }],
+            nice: true,
+            zero: true,
+            domain: { data: 'distribution', field: 'amount' },
+          },
+        ],
+
+        axes: [
+          {
+            orient: 'bottom',
+            scale: 'x',
+            format: 's',
+            title: 'Cluster B',
+            tickCount: 5,
+          },
+        ],
+
+        marks: [
+          {
+            type: 'rect',
+            from: { data: 'selectionB' },
+            encode: {
+              enter: {
+                x: { scale: 'x', field: 'amount' },
+                x2: { scale: 'x', value: 0 },
+                y: { scale: 'y', field: 'label' },
+                height: { scale: 'y', band: 1, offset: -1 },
+                fillOpacity: { value: 1 },
+                fill: { scale: 'c', field: 'selection' },
+                tooltip: {
+                  signal:
+                    "{'title': 'Selection B','category': datum.label, 'amount': datum.amount}",
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
   };
 </script>
 
-<VegaLite {data} {spec} />
+<Vega {spec} />
