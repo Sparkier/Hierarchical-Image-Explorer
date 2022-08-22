aaac1f0c6cf09df3a1e1cc324a6e0980f80d250f
nik-weidl <69912534+nik-weidl@users.noreply.github.com>
2022-08-22 16:31:00 +0200

    fix: colors

--- hierarchical-image-explorer/src/components/minis/PieChart.svelte
+++ hierarchical-image-explorer/src/components/minis/PieChart.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import { VegaLite } from 'svelte-vega';
   import { ColorUtil } from '../../services/colorUtil';
-  import { selectedColorPalette } from '../../stores';
 
   export let data: { label: string; amount: number }[] = [];
-  let colorPalette: string;
 
   $: distribution = [
     ...data.map((e) => {
@@ -15,17 +13,13 @@
     }),
   ];
 
-  selectedColorPalette.subscribe((v) => {
-    colorPalette = v;
-  });
-
   $: colorscheme =
     distribution == undefined
       ? []
       : distribution
           .map((d) => d.category)
           .sort()
-          .map((e) => ColorUtil.getColor(e, colorPalette));
+          .map((e) => ColorUtil.getColor(e, ''));
 
   $: spec = {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
@@ -39,7 +33,6 @@
       theta: { field: 'amount', type: 'quantitative', stack: 'normalize' },
       color: {
         field: 'category',
-        type: 'nominal',
         scale: { range: colorscheme },
       },
     },
--- hierarchical-image-explorer/src/stores.ts
+++ hierarchical-image-explorer/src/stores.ts
@@ -4,8 +4,8 @@ import type { HexagonPropertiesMap, QuantizationResults } from './types';
 export const currentQuantization: Writable<null | QuantizationResults> =
   writable(null);
 export const hexagonPropertiesMap: Writable<HexagonPropertiesMap> = writable({
-  color: 'd => op.any(d.id)',
-  representantID: 'd => op.mode(d.id)',
+  color: 'd => op.any(d.label)',
+  representantID: 'd => op.mode(d.label)',
 });
 export const colorPropertyType: Writable<string> = writable('');
 export const selectedColorPalette: Writable<string> = writable('');
